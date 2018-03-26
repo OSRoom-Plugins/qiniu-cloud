ffb2a9631dbc046acb40ff85d0164a5dc38c7988
HiWoo <xiaopingwoo@163.com>
2018-03-26 20:31:47 +0800

    Update

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 __author__ = "Allen Woo"
 
-ACCESS_KEY = "yizLx8RpVKvOi7UqJpH4d4wnlUSwHNojG-5qOXU5"
-SECRET_KEY = "qvzGv02GS2LGEQwLmx0PYPryPJBfBkSkhe9Ph5dg"
+ACCESS_KEY = "Your AK"
+SECRET_KEY = "Your SK"
 LOCAL_TEMP_FOLDER = "upload_temp"
 BUCKET_NAME = "osroom-test2"
 DOMAIN = "http://p66v9rn28.bkt.clouddn.com"
