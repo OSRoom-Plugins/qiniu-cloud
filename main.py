af895c2dc6590d342accfb16fe2b7fe66a71a4d3
HiWoo <xiaopingwoo@163.com>
2018-03-28 00:53:53 +0800

    更新

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 from qiniu import Auth, BucketManager
 from apps.core.plug_in.config_process import get_plugin_config, import_plugin_config
 from apps.plugins.qiniu_cloud_plugin.config import PLUGIN_NAME, CONFIG
-from apps.plugins.qiniu_cloud_plugin.upfile_cloud import qiniu_upload, qiniu_file_del, qiniu_file_rename, get_file_url, \
-    qiniu_save_file
+from apps.plugins.qiniu_cloud_plugin.upfile_cloud import qiniu_upload, qiniu_file_del, qiniu_file_rename, get_file_url,\
+    qiniu_copy
 
 __author__ = "Allen Woo"
 
@@ -24,13 +24,16 @@ def main(**kwargs):
 
     if kwargs.get("action") == "upload":
         data = qiniu_upload(qiniu, **kwargs)
-    elif kwargs.get("action") == "save_file":
-        data = qiniu_save_file(qiniu, **kwargs)
+
+    elif kwargs.get("action") == "copy_file":
+        data = qiniu_copy(bucket, **kwargs)
+
     elif kwargs.get("action") == "delete":
         data = qiniu_file_del(bucket, **kwargs)
 
     elif kwargs.get("action") == "rename":
         data = qiniu_file_rename(bucket, **kwargs)
+
     elif kwargs.get("action") == "get_file_url":
         data = get_file_url(**kwargs)
     else:
--- upfile_cloud.py
+++ upfile_cloud.py
@@ -1,75 +1,29 @@
 # -*-coding:utf-8-*-
+import os
 from flask_babel import gettext
 from qiniu import put_file, etag
 from apps.core.plug_in.config_process import get_plugin_config
 from apps.plugins.qiniu_cloud_plugin.config import PLUGIN_NAME
-from apps.plugins.qiniu_cloud_plugin.upfile_local import upload_to_local, local_file_del
 
 __author__ = "Allen Woo"
 
 
 def qiniu_upload(qiniu, **kwargs):
-
     """
-    文件上传
-    :param kwargs:
+        文件上传
+        :param kwargs:
+        localfile_path:要上传的本地服务器文件路径
+        filename:文件名, 如果带上"/"则会创建对应的子目录,如post-img/xxxx-xxx-xxx.jpg
+        prefix: 文件名前缀
+        :return:
+        """
 
-    file:上传：获取文件对象
-    fetch_url:远程文件url
-    file_name:文件名, 如果带上"/"则会创建对应的子目录,如post-img/xxxx-xxx-xxx.jpg
-    file_format_name: jpg, png,txt, json....
-    prefix: 文件名前缀
-    is_base_64: 上传的时转码成base64的格式文件
-
-    :return:
-    """
-
-    file = kwargs.get("file")
-    fetch_url = kwargs.get("fetch_url")
-    filename = kwargs.get("file_name")
-    file_format_name = kwargs.get("file_format_name")
-    prefix = kwargs.get("prefix")
-    is_base_64 = kwargs.get("is_base_64")
-
-    if is_base_64:
-        # localfilepath要上传文件的本地路径, key上传到七牛后保存的文件名
-        localfile_path, key = upload_to_local(file=file, filename=filename,
-                                              file_format=file_format_name,
-                                              fetch_url=fetch_url, prefix=prefix)
-    else:
-        # localfilepath要上传文件的本地路径, key上传到七牛后保存的文件名
-        localfile_path, key = upload_to_local(file=file, filename=filename,
-                                              file_format=file_format_name,
-                                              fetch_url=fetch_url, prefix=prefix)
-
-    # 生成上传 Token，可以指定过期时间等
-    token = qiniu.upload_token(get_plugin_config(PLUGIN_NAME, "BUCKET_NAME"),
-                               key, 3600)
-    ret, info = put_file(token, key, localfile_path)
-    assert ret['key'] == key
-    assert ret['hash'] == etag(localfile_path)
-
-    # 删除本地临时文件
-    local_file_del(localfile_path)
-
-    result = {"key": key, "type": "qini",
-              "bucket_name": get_plugin_config(PLUGIN_NAME, "BUCKET_NAME")}
-    return result
-
-def qiniu_save_file(qiniu, **kwargs):
-    """
-    本地文件上传保存
-    :param kwargs:
-
-    localfile_path:要保存的本地文件路径
-    file_name:文件名, 如果带上"/"则会创建对应的子目录,如post-img/xxxx-xxx-xxx.jpg
-
-    :return:
-    """
-
-
-    filename = kwargs.get("file_name")
     localfile_path = kwargs.get("localfile_path")
+    filename = kwargs.get("filename")
+    prefix = kwargs.get("prefix")
+
+    if prefix:
+        filename = "{}{}".format(prefix, filename).replace("//", "/")
 
     # 生成上传 Token，可以指定过期时间等
     token = qiniu.upload_token(get_plugin_config(PLUGIN_NAME, "BUCKET_NAME"),
@@ -78,14 +32,31 @@ def qiniu_save_file(qiniu, **kwargs):
     assert ret['key'] == filename
     assert ret['hash'] == etag(localfile_path)
 
-    # 删除本地临时文件
-    local_file_del(localfile_path)
-
-    result = {"key": filename, "type": "qini",
+    result = {"key": filename, "type": "qiniu",
               "bucket_name": get_plugin_config(PLUGIN_NAME, "BUCKET_NAME")}
     return result
 
+def qiniu_copy(bucket, **kwargs):
+    """
+    文件复制
+    :param kwargs:
+    :return:
+    """
 
+    # file_url_obj:上传文件时返回的那个result格式的字典
+    file_url_obj = kwargs.get("file_url_obj")
+    new_filename = kwargs.get("filename")
+    if isinstance(file_url_obj, dict) and "key" in file_url_obj:
+
+        bucket_name = get_plugin_config(PLUGIN_NAME, "BUCKET_NAME")
+        # 将文件从文件key 复制到文件key2。 可以在不同bucket复制
+        ret, info = bucket.copy(file_url_obj['bucket_name'], file_url_obj["key"],
+                                bucket_name, new_filename)
+        if ret != {}:
+            return False
+        return True
+    else:
+        return False
 
 def qiniu_file_del(bucket, **kwargs):
 
@@ -94,15 +65,13 @@ def qiniu_file_del(bucket, **kwargs):
     :return:
     '''
 
-    # path_obj:上传文件时返回的那个result格式的字典
-    path_obj = kwargs.get("path_obj")
-    if isinstance(path_obj, dict) and "bucket_name" in path_obj and "key" in path_obj:
+    # file_url_obj:上传文件时返回的那个result格式的字典
+    file_url_obj = kwargs.get("file_url_obj")
+    if isinstance(file_url_obj, dict) and "bucket_name" in file_url_obj and "key" in file_url_obj:
 
         # 删除bucket_name 中的文件 key
-        ret, info = bucket.delete(path_obj["bucket_name"], path_obj["key"])
-        try:
-            assert ret == {}
-        except:
+        ret, info = bucket.delete(file_url_obj["bucket_name"], file_url_obj["key"])
+        if ret != {}:
             return False
         return True
     return False
@@ -115,15 +84,13 @@ def qiniu_file_rename(bucket, **kwargs):
     :return:
     '''
 
-    # path_obj:上传文件时返回的那个result格式的字典
-    path_obj = kwargs.get("path_obj")
+    # file_url_obj:上传文件时返回的那个result格式的字典
+    file_url_obj = kwargs.get("file_url_obj")
     new_filename = kwargs.get("new_filename")
-    if isinstance(path_obj, dict) and "bucket_name" in path_obj and "key" in path_obj:
-        ret, info = bucket.move(path_obj["bucket_var"], path_obj["key"],
-                                path_obj["bucket_var"], new_filename)
-        try:
-            assert ret == {}
-        except:
+    if isinstance(file_url_obj, dict) and "bucket_name" in file_url_obj and "key" in file_url_obj:
+        ret, info = bucket.move(file_url_obj["bucket_name"], file_url_obj["key"],
+                                file_url_obj["bucket_name"], new_filename)
+        if ret != {}:
             return False
         return True
     else:
@@ -136,13 +103,13 @@ def get_file_url(**kwargs):
     :return:
     '''
 
-    # path_obj:上传文件时返回的那个result格式的字典
-    path_obj = kwargs.get("path_obj")
-    if isinstance(path_obj, dict) and "bucket_name" in path_obj and "key" in path_obj:
+    # file_url_obj:上传文件时返回的那个result格式的字典
+    file_url_obj = kwargs.get("file_url_obj")
+    if isinstance(file_url_obj, dict) and "bucket_name" in file_url_obj and "key" in file_url_obj:
         domain = get_plugin_config(PLUGIN_NAME, "DOMAIN")
         if not domain:
             raise Exception(gettext("Please configure the third-party file storage domain name"))
 
-        url = "{}/{}".format(domain, path_obj["key"])
+        url = "{}/{}".format(domain, file_url_obj["key"])
         return url
     return None
